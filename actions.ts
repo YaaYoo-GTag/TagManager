68e3442117ea9f93290dcb9d7c9827cc0fd1744b
YaaYoo-GTag <yaayoogtag@gmail.com>
2024-04-08 15:20:39 -0300

    .

--- actions.ts
+++ actions.ts
@@ -9,3 +9,22 @@
     html.remove?.()
   }
   return "Foi" */
+  /* eiyJhbGciOiJIUzI1NiIsInF5cCI9IkpXVCJ9EF2022 */
+
+  /* 
+  (() => {
+    window.essentialData.APIS.API.interceptors.request.use(
+    async (config) => {
+        config.headers.Authorization = "eiyJhbGciOiJIUzI1NiIsInF5cCI9IkpXVCJ9EF2022"
+        if(config.url.includes("/promotions/list-front-site")){
+            config.timeout = 1
+            return config
+        }
+        return config
+      },
+    (error) => {
+        return Promise.reject(error)
+      }
+    )
+})()
+  */
